a80011844e21d1b7d6a3776ab47e3e8f15b1b193
= <romarks@ucsc.edu>
2016-09-18 04:04:19 +0000

    working version

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var rules =[[1,1,1],[0,0,0],[1,0,0]];
 
 window.onload = function(){
     generateFirstRow();
-    generateRow();
+    setInterval(function(){
+        generateRow();
+    },500);
 }
 
 function generateFirstRow(){
@@ -25,24 +27,23 @@ function generateFirstRow(){
 
 function generateRow(){
     var row = document.createElement('div');
-    for (var i=0; i<numColumn; i++){
+    for (var i=0; i<numColumns; i++){
         var cell = document.createElement('div');
         var lastCell = state(lastRowCreated.children[i]);
         if (i==0){
-            var prevCell = state(lastRowCreated.children[numColumn-1]);
+            var prevCell = state(lastRowCreated.children[numColumns-1]);
         } else {
             var prevCell = state(lastRowCreated.children[i-1]);
         }
-        if (i=numColumn-1){
+        if (i==numColumns-1){
             var nextCell = state(lastRowCreated.children[0]);
         } else {
             var nextCell = state(lastRowCreated.children[i+1]);
         }
-        alert(prevCell,lastCell,nextCell);
         if (matchesRules(prevCell, lastCell, nextCell)){
-            cell.className = 'cell active';
-        } else {
             cell.className = 'cell';
+        } else {
+            cell.className = 'cell active';
         }
         row.appendChild(cell);
     }
